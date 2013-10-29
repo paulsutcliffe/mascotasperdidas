e2cbae507c8c054b9e39e9b61463f00dd9581aa6
adriel <apinzasm@gmail.com>
2013-10-29 15:42:10 -0500

    Creada la aplicacion

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mascotasperdidas::Application.config.secret_token = 'abec880fd3a6c9f2dbe25f4b38331ec4da5f01bc9b90e1a4a634a14d06d44259f96bf7de3d6a01fd2339463e50940d120a74a9eb0ca7aad371bde183370960ec'
